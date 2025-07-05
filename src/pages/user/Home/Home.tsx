deb8be85ff7b97ffcebc29b273a733d40bf32f1a
hm32295 <hm32295@gmail.com>
2025-07-05 19:11:47 +0300

    handel token

--- src/pages/user/Home/Home.tsx
+++ src/pages/user/Home/Home.tsx
@@ -1,8 +1,14 @@
+import { useContext } from "react"
+import { AuthContext } from "../../../context/context"
 
 const Home = () => {
+  const{loginData} = useContext(AuthContext );
+  console.log(loginData);
+  
   return (
     <div>
    Home
+    
     </div>
   )
 }
